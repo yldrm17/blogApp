2b25bb4daf9db5493868a2480c744fb2779276d2
enesyldrm <enes082317@gmail.com>
2023-10-22 17:01:14 +0300

    first commit

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import BlogContext from '../context/BlogContext';
+
+const IndexScreen = () => {
+  const value = useContext(BlogContext);
+
+  return (
+    <View>
+      <Text>Index Sayfasi</Text>
+      <Text>{value}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
