7d25fa1d667aa94bc350c5aaf41d40dad5727085
enesyldrm <enes082317@gmail.com>
2023-10-29 16:11:39 +0300

    touchable blog and createScreen

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const CreateScreen = ({navigation}) => {
+    return(
+        <View>
+            <Text>Create</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
-import { Entypo } from '@expo/vector-icons';
+import { Entypo,  } from '@expo/vector-icons';
 
-const IndexScreen = () => {
+const IndexScreen = ({navigation}) => {
   const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
   return (
@@ -14,14 +14,16 @@ const IndexScreen = () => {
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
           return(
-            <View style={styles.row}> 
-              <Text style={styles.title}>
-               {item.title} - {item.id}
-              </Text>
-              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-               <Entypo name="trash" size={24} color="black" />
-              </TouchableOpacity> 
-            </View>
+            <TouchableOpacity onPress={() => navigation.navigate('Show', {id: item.id})}>
+              <View style={styles.row}> 
+                <Text style={styles.title}>
+                  {item.title} - {item.id}
+                </Text>
+                  <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                <Entypo name="trash" size={24} color="black" />
+                </TouchableOpacity> 
+              </View>
+            </TouchableOpacity>
           );
         }}
       />
